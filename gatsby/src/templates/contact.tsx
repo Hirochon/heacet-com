f06039bc6bad3102eccba7ad5e2fed2811182cf4
Hirochon <heacet43@gmail.com>
2021-01-02 13:12:06 +0900

    [add] 押したくなるボタンを追加

--- gatsby/src/templates/contact.tsx
+++ gatsby/src/templates/contact.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import "./post.scss";
 
 
 const ContactForm = () => (
@@ -16,25 +15,27 @@ const ContactForm = () => (
 
       <div className="form-group">
         <label>お名前(必須)<abbr title="required"></abbr>
-        <input type="text" className="form-control" id="name" name="name" placeholder="お名前" maxLength={30} minLength={2} required autoComplete="name" />
+        <br/>
+        <input type="text" className="form-control" id="name" name="name" maxLength={30} minLength={2} required autoComplete="name" />
         </label>
       </div>
       <div className="form-group">
         <label>メールアドレス(必須)<abbr title="required"></abbr>
+        <br/>
         <input type="email" className="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autoComplete="email" />
         </label>
       </div>
       <div className="form-group">
         <label>お問い合わせ内容(必須)<abbr title="required"></abbr>
+        <br/>
         <textarea className="form-control" id="contact" name="content" rows={8} required></textarea>
         </label>
       </div>
 
       <div className="form-group">
-      <button type="submit">送信</button>
+      <button className="btn btn--orange btn--cubic btn--shadow" type="submit">送信</button>
       </div>
     </form>
-    <Link to="/">home</Link>
   </div>
 )
 
